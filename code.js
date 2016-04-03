adc74ffa3c0cded28bcd8163add032be900e117f
Dax <daxfdr@gmail.com>
2016-04-03 18:28:59 -0300

    Exemplo para tutorial Github SóQuestões

--- code.js
+++ code.js
@@ -1,11 +1,17 @@
+//Esta função retorna o primeiro nome da pessoa
 $scope.nome = function(param){
-	console.log('Mensagem');
 	return param
 };
+//Esta função retorna o sobrenome da pessoa
 $scope.sobrenome = function(param){
 	return param
 };
 
+//Esta função retorna o nome completo da pessoa:
+//         nome + sobrenome
 $scope.nomeCompleto = function(param1, param2){
-	return $scope.nome('João') + ' ' + $scope.sobrenome('Silveira');
-}
+	return $scope.nome(param1) + ' ' + $scope.sobrenome(param2);
+};
+
+//Esta variável recebe o retorno da função nomeCompleto
+var nomePessoa = $scope.nomeCompleto('João', 'Silveira');
